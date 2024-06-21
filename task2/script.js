affd5b72184698e2df7af5ea6efadf436cdb51c3
Nikita Kichaykin <unknol@outlook.com>
2024-06-21 21:37:45 +1000

    Add task2

---
+++ task2/script.js
@@ -0,0 +1,7 @@
+const consolelog = document.querySelector('#consolelog');
+
+document.querySelector('#consolelog').addEventListener('click', function (event){
+    
+    alert('Пример использования кнопки console.log');
+    console.log('console.log');
+});
