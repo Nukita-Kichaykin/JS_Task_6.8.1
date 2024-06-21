575e85d99b395f75e2a992686c69ae339f617892
Nikita Kichaykin <unknol@outlook.com>
2024-06-21 22:00:56 +1000

    chenge task2/script.js

--- task2/script.js
+++ task2/script.js
@@ -1,7 +1,22 @@
 const consolelog = document.querySelector('#consolelog');
+const alertt = document.querySelector('#alert')
+const promptt = document.querySelector('#prompt')
 
-document.querySelector('#consolelog').addEventListener('click', function (event){
+    document.querySelector('#consolelog').addEventListener('click', function (event){
     
-    alert('Пример использования кнопки console.log');
-    console.log('console.log');
+        alert('Пример использования кнопки console.log');
+        console.log('console.log');
+});
+
+    document.querySelector('#alert').addEventListener('click', function (event){
+    
+        alert('Пример использования кнопки alert');
+        console.log('alert()');
+});
+
+    document.querySelector('#prompt').addEventListener('click', function (event){
+    
+        alert('Пример использования кнопки prompt');
+        prompt('Введите текст!');
+        console.log('prompt');
 });
